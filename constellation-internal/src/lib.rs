817a8760a0b62e4656e37bbec2609ad6df4e3015
alecmocatta <alec@mocatta.net>
2019-09-04 15:19:58 +0100

    v0.1.5

--- constellation-internal/src/lib.rs
+++ constellation-internal/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/constellation-internal/0.1.4")]
+#![doc(html_root_url = "https://docs.rs/constellation-internal/0.1.5")]
 #![warn(
 	// missing_copy_implementations,
 	missing_debug_implementations,
