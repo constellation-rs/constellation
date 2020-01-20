ea9216ce111c1fe927351d498467232e942c7306
alecmocatta <alec@mocatta.net>
2020-01-20 00:21:02 +0000

    v0.1.10

--- constellation-internal/src/lib.rs
+++ constellation-internal/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/constellation-internal/0.1.9")]
+#![doc(html_root_url = "https://docs.rs/constellation-internal/0.1.10")]
 #![warn(
 	// missing_copy_implementations,
 	missing_debug_implementations,
--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@
 //!
 //! The only requirement to use is that [`init()`](init) must be called immediately inside your application's `main()` function.
 
-#![doc(html_root_url = "https://docs.rs/constellation-rs/0.1.9")]
+#![doc(html_root_url = "https://docs.rs/constellation-rs/0.1.10")]
 #![cfg_attr(feature = "nightly", feature(read_initializer))]
 #![warn(
 	missing_copy_implementations,
